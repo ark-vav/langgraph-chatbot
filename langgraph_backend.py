053f1ef6c64ae85ebf4bda7783e8f89e2c3e6a05
Vaibhav Ghildiyal <vaibhavghildiyal394@gmail.com>
2026-07-17 03:23:20 +0530

    Chats Threading

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_groq import ChatGroq
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
@@ -30,7 +30,9 @@ graph.add_node("chat_node", chat_node)
 graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
-chatbot = graph.compile(checkpointer=checkpointer) 
+chatbot = graph.compile(checkpointer=checkpointer)
+
+
 
 
 
